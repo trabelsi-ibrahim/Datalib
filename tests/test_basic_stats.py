cba0130e15c4aa21b2db9deb53c35e11cc2250a7
trabelsi-ibrahim <ib03092001@gmail.com>
2024-12-19 22:09:32 +0100

    alpha.2

--- tests/test_basic_stats.py
+++ tests/test_basic_stats.py
@@ -1,7 +1,10 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
 import pytest
 import pandas as pd
-import datalib
-#from datalib.statslib.basic_stats import calculate_mode, calculate_std, calculate_correlation
+from datalib.statslib.basic_stats import calculate_mode, calculate_std, calculate_correlation
 
 @pytest.fixture
 def sample_mode_data():
